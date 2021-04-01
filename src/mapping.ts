607cbf365d2092e30c3bed18fb037ba38f0fd676
Dylan <dylankilkenny95@gmail.com>
2021-04-01 19:57:37 +0100

    Graph - update graph

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,20 +1,71 @@
-import { CellMinted, URI } from "../generated/Muralis/Muralis";
-import { Cell } from "../generated/schema";
-import { log } from "@graphprotocol/graph-ts";
+import { CellMinted, URI, TransferSingle } from "../generated/PxlGen/PxlGen";
+import { Token, Owner, TokenTransfer } from "../generated/schema";
+import { log, Address, BigInt } from "@graphprotocol/graph-ts";
+
+const CELL_TYPE: string = "Cell";
+const PRINT_TYPE: string = "Print";
+const ZERO_ADDR: string = "0x0000000000000000000000000000000000000000";
+
+export function getOwner(o: string): Owner {
+  let owner = Owner.load(o);
+  if (owner == null) {
+    owner = new Owner(o);
+    owner.totalOwned = 0;
+  }
+  return owner as Owner;
+}
 
 export function handleCellMinted(event: CellMinted): void {
   log.info("Cell ID: {}", [event.params.id.toHex()]);
-  let cell = new Cell(event.params.id.toHex());
-  cell.owner = event.params.to;
+  log.info("Cell Index: {}", [event.params.index.toString()]);
+
+  let ownerID = event.params.to.toHex();
+  let owner = getOwner(ownerID);
+  owner.totalOwned = owner.totalOwned + 1;
+
+  let cell = new Token(event.params.id.toHex());
+  cell.owner = ownerID;
   cell.index = event.params.index.toI32();
-  cell.ipfsHash = event.params.tokenuri;
+  cell.ipfsHash = event.params.uri + "/" + event.params.index.toString() + ".json";
+  cell.createdAt = event.block.timestamp;
+  cell.type = CELL_TYPE;
+
   cell.save();
+  owner.save();
 }
 
 export function handleUpdatedURI(event: URI): void {
   log.info("URI ID: {}", [event.params.id.toHex()]);
   let id = event.params.id.toHex();
-  let cell = Cell.load(id);
+  let cell = Token.load(id);
   cell.ipfsHash = event.params.value;
   cell.save();
+}
+
+export function handleTransferSingle(event: TransferSingle): void {
+  if (event.params.from.toHexString() !== ZERO_ADDR || event.params.to.toHexString() !== ZERO_ADDR) {
+    let tokenId = event.params.id.toHex();
+    let token = Token.load(tokenId);
+    if (token !== null) {
+      let oldOwnerID = event.params.from.toHex();
+      let oldOwner = getOwner(oldOwnerID);
+      oldOwner.totalOwned = oldOwner.totalOwned - 1;
+      oldOwner.save();
+
+      let newOwnerID = event.params.to.toHex();
+      let newOwner = getOwner(newOwnerID);
+      newOwner.totalOwned = newOwner.totalOwned + 1;
+      newOwner.save();
+
+      token.owner = newOwnerID;
+      token.save();
+
+      let transfer = new TokenTransfer(event.transaction.hash.toHex());
+      transfer.token = tokenId;
+      transfer.to = event.params.to;
+      transfer.from = event.params.from;
+      transfer.createdAt = event.block.timestamp;
+      transfer.save();
+    }
+  }
 }
